6d6a6215cc8d5e1b56f75757b606dec6fd145361
Gulistan Saglam <gulistansglm@gmail.com>
2023-10-26 23:55:08 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import { useState } from 'react';
+
+import { CORE_CONCEPTS } from './data';
+import Header from './components/Header/Header.jsx';
+import CoreConcept from './components/CoreConcept';
+import TabButton from './components/TabButton';
+import { EXAMPLES } from './data';
+
+
+
+function App() {
+  const [ selectedTopic, setSelectedTopic ] = useState();
+
+  function handleSelect(selectedButton) {
+    //selectedButton => 'components', 'jsx', 'props', 'state'
+    setSelectedTopic(selectedButton);
+    console.log(selectedTopic);
+  }
+
+  return (
+    <div>
+      <Header />
+      <main>
+
+       <section id="core-concepts">
+        <h2>Core Concepts</h2>
+        <ul>
+          {CORE_CONCEPTS.map((conceptItem)=> (
+            <CoreConcept key={conceptItem.title} {...conceptItem}/>
+          ))}
+        </ul>
+        </section>
+
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() =>handleSelect('components')}>Components</TabButton>
+            <TabButton onSelect={() =>handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() =>handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={() =>handleSelect('state')}>State</TabButton>
+          </menu>
+          
+            {!selectedTopic && <p>Please select a topic!</p>} 
+            {selectedTopic && (
+            <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>
+              {EXAMPLES[selectedTopic].code}
+              </code>
+            </pre>
+          </div> 
+          )} 
+        </section>
+
+      </main>
+    </div>
+  );
+}
+
+export default App;
